e2d404d9a93cae5cca3555643e500f9f96d18d20
shemanto27 <shemantosharkarofficial@gmail.com>
2025-03-08 11:59:46 +0600

    backend and frontend setup done

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button class="btn btn-primary">One</button>
+    </div>
+  )
+}
+
+export default Header
