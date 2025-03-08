1d0e5db0153f7636b0d39c9cd521a383031b6c52
shemanto27 <shemantosharkarofficial@gmail.com>
2025-03-08 15:27:51 +0600

    navbar, hero section added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> 
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> 
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li><a>Blogs</a></li>
+              <li><a>Free Tools</a></li>
+              <li><a>Books</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Shemanto Sharkar</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>Blogs</a></li>
+            <li><a>Free Tools</a></li>
+            <li><a>Books</a></li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a href="https://www.linkedin.com/in/your-linkedin-profile" className="btn btn-ghost">
+            <FaLinkedin className="h-5 w-5" />
+          </a>
+          <a href="https://github.com/your-github-profile" className="btn btn-ghost">
+            <FaGithub className="h-5 w-5" />
+          </a>
+          <a className="btn">Login</a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './Header'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header></Header>
+    <Navbar></Navbar>
+    <Hero></Hero>
   </StrictMode>,
 )
