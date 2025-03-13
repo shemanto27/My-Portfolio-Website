00f4ae8eb32e7c53153dcac3af94e1a132e5044c
shemanto27 <shemantosharkarofficial@gmail.com>
2025-03-13 18:08:26 +0600

    form added, problem didnot solve

--- backend/blogsapp/models.py
+++ backend/blogsapp/models.py
@@ -3,7 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
+    CATEGORY_CHOICES = [
+        ('Django', 'Django'),
+        ('Machine Learning', 'Machine Learning'),
+        ('React', 'React'),
+        ('Computer Vision', 'Computer Vision'),
+        ('Web Development', 'Web Development'),
+        ('Data Science', 'Data Science'),
+        ('MLOps', 'MLOps'),
+        ('Business', 'Business'),
+        ('Others', 'Others'),
+        ('Project', 'Project'),
+    ]
+
+    cover_image = models.ImageField("Cover Image", upload_to="images/", null=True, blank=True)
     title = models.CharField("Title", max_length=100)
+    category = models.CharField("Category", max_length=50, choices=CATEGORY_CHOICES, default='Project')
     body = models.TextField("Blog Content")
     created_at = models.DateTimeField("Published Date", auto_now_add=True)
 
